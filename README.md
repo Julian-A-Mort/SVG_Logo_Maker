c412f7cfc8aef015b89aa3b3ea996541e4eacef6
Julian <julian@wheresnick.com.au>
2023-10-30 15:30:11 +1100

    fixed prompt and installed older version of inquirer

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # SVG_Logo_Maker
+
+step 1 install inquirer npm install inquirer
+npm install inquirer@7.3.3
+
--- index.js
+++ index.js
@@ -1,42 +1,52 @@
 const inquirer = require('inquirer');
 
-inquirer
-  .prompt({
-    type: 'input',
-    name: 'logoAcronym',
-    message: 'Enter Three Letters:',
-    validate: function(input) {
-        const pattern = /^[a-zA-Z]{3}$/;
-        if (pattern.test(input)) {
-          return true;
-        }
-        return 'I said three letters!';
+inquirer.prompt({
+  type: 'input',
+  name: 'logoAcronym',
+  message: 'Enter Three Letters:',
+  validate: function(input) {
+    const pattern = /^[a-zA-Z]{3}$/;
+    if (pattern.test(input)) {
+      return true;
     }
-}).then (() => {
-    return inquirer.prompt({
-        type: 'input',
-        name: 'colourInput',
-        message: 'Enter Colour',
-        validate: function(input) {
-            const colorPattern = /^#(?:[0-9a-fA-F]{3}){1,2}$/;
-            const colorKeywords = ["red", "orange", "yellow", "green", "blue", "indigo", "violet", "black", "white"];
-            if (colorPattern.test(input) || colorKeywords.includes(input.toLowerCase())) {
-              return true;
-            }
-            return 'Please enter a valid color keyword or hexadecimal color code.';
-          }
-    });
-}).then (() => {
-    return inquirer.prompt({
-        type: 'input',
-        name: 'shapeInput',
-        message: 'Enter Shape',
-        validate: function(input) {
-            const shapeKeywords = ["circle", "triangle", "square"];
-            if (shapeKeywords.includes(input.toLowerCase())) {
-                return true;
-              }
-              return 'Please choose from Circle, Triangle or Square!';          }
-    });
+    return 'I said three letters!';
+  }
+})
+.then(answers => {
+  return inquirer.prompt({
+    type: 'input',
+    name: 'colourInput',
+    message: 'Enter Colour:',
+    validate: function(input) {
+      const colorPattern = /^#(?:[0-9a-fA-F]{3}){1,2}$/;
+      const colorKeywords = ["red", "orange", "yellow", "green", "blue", "indigo", "violet", "black", "white"];
+      if (colorPattern.test(input) || colorKeywords.includes(input.toLowerCase())) {
+        return true;
+      }
+      return 'Please enter a valid color keyword or hexadecimal color code.';
+    }
+  })
+  .then(colourAnswer => {
+    return {...answers, ...colourAnswer};
+  });
+})
+.then(answers => {
+  return inquirer.prompt({
+    type: 'input',
+    name: 'shapeInput',
+    message: 'Enter Shape:',
+    validate: function(input) {
+      const shapeKeywords = ["circle", "triangle", "square"];
+      if (shapeKeywords.includes(input.toLowerCase())) {
+        return true;
+      }
+      return 'Please choose from Circle, Triangle, or Square!';
+    }
+  })
+  .then(shapeAnswer => {
+    return {...answers, ...shapeAnswer};
+  });
+})
+.then(finalAnswers => {
+  console.log('All answers:', finalAnswers);
 });
-
